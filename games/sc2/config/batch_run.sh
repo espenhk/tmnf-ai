8595e06b0f382dee5601472b777eae79d07cef15
Espen H. Kristensen <espenhkristensen@gmail.com>
2026-05-08 08:35:33 +0200

    Grid search tweaks, add batch_run script

---
+++ games/sc2/config/batch_run.sh
@@ -0,0 +1,5 @@
+poetry run python grid_search.py --game sc2 games/sc2/config/gs_mcts_game_template.yaml --no-interrupt
+poetry run python grid_search.py --game sc2 games/sc2/config/gs_sc2_cnn_template.yaml --no-interrupt
+poetry run python grid_search.py --game sc2 games/sc2/config/gs_genetic_template.yaml --no-interrupt
+poetry run python grid_search.py --game sc2 games/sc2/config/gs_neural_dqn_template.yaml --no-interrupt
+poetry run python grid_search.py --game sc2 games/sc2/config/gs_reinforce_template.yaml --no-interrupt
