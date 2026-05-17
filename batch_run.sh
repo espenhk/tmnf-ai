15969a04fc2f833d6f233acd612fefca548ab80e
Espen H. Kristensen <espenhkristensen@gmail.com>
2026-05-18 01:58:46 +0200

    merge and update grid searches

--- batch_run.sh
+++ batch_run.sh
@@ -1,5 +1,4 @@
+poetry run python grid_search.py --game sc2 games/sc2/config/gs_reinforce_template.yaml --no-interrupt
 poetry run python grid_search.py --game sc2 games/sc2/config/gs_mcts_game_template.yaml --no-interrupt
 poetry run python grid_search.py --game sc2 games/sc2/config/gs_genetic_template.yaml --no-interrupt
 poetry run python grid_search.py --game sc2 games/sc2/config/gs_neural_dqn_template.yaml --no-interrupt
-poetry run python grid_search.py --game sc2 games/sc2/config/gs_reinforce_template.yaml --no-interrupt
-poetry run python grid_search.py --game sc2 games/sc2/config/gs_cmaes_template.yaml --no-interrupt
